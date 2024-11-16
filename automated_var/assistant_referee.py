e1d09486f5a67c144e13ecaf6758d71b576b2283
Yusuf Sulehman <yusuf.sulehman@postgrad.manchester.ac.uk>
2024-11-16 18:45:35 +0000

    moved

--- automated_var/assistant_referee.py
+++
@@ -1,7 +0,0 @@
-from automated_var.src.assistant_var_functionalities import mistaken_identity_checker as mic
-from automated_var.src.assistant_var_functionalities import offside_checker as oc
-
-
-class AssistantReferee(mic.IdentityCheck, oc.OffsideCheck):
-    def __init__(self):
-        pass

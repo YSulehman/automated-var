988ef576abc7c314e26ffb6ab87261b37827b0d9
Yusuf Sulehman <yusuf.sulehman@postgrad.manchester.ac.uk>
2024-11-23 13:41:18 +0000

    logic added for loading model

--- assistant_referee.py
+++ assistant_referee.py
@@ -1,3 +1,4 @@
+import os
 import yolov5
 import argparse
 from automated_var.src.assistant_var_functionalities import mistaken_identity_checker as mic
@@ -9,8 +10,17 @@ from automated_var.src.assistant_var_functionalities import offside_checker as o
 #         pass
 
 def main(args):
-    # load the YOLOV5s model
-    model = yolov5.load_model("yolov5s")
+    # need to add in functionality for loading fine-tuned yolo model
+    if args.fine_tuned_model:
+        # check if the fine-tuned model exists
+        if not os.path.exists(args.fine_tuned_model):
+            raise FileNotFoundError(f"File {args.fine_tuned_model} not found")
+        else:
+            # load the fine-tuned model
+            model = yolov5.load_model(args.fine_tuned_model)
+    else:
+        # load the YOLOV5s pre-trained model
+        model = yolov5.load_model("yolov5s.pt")
 
     if args.command == "identity-check":
         var = mic.IdentityCheck()
@@ -29,6 +39,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-com", "--command", type=str, choices=["identity-check", "offside-check"], required=True, help="which VAR functionality to run")
+    parser.add_argument("-ftm", "--fine-tuned-model", type=str, help="path to the fine-tuned YOLOV5 model")
     parser.add_argument("-sv", "--source-video", type=str, required=True, help="path to the input video")
     parser.add_argument("-dv", "--destination-video", type=str, help="path to the output video")
 
